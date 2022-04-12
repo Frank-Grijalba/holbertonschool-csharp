d436835d0e559ca27f4681154acebc8c7696b10d
Frank-Grijalba <fjgrijalba@gmail.com>
2022-04-11 20:18:41 -0500

    update tsk 9

--- 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-
 class Program
 {
     static void Main(string[] args)
     {
         double percent = .7553;
         double currency = 98765.4321;
-        Console.WriteLine($"Percent: {percent:0.00%} tip\nCurrency: {currency:C}");
+        Console.WriteLine($"Percent: {percent:0.00%}\nCurrency: {currency:C}");
     }
 }
