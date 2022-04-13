a41255219296b331d93855f31e86e589a4094a1d
Frank-Grijalba <fjgrijalba@gmail.com>
2022-04-12 21:33:54 -0500

    Tsk 6-7

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int num1=0; num1 < 9; num1++){
+                for (int num2=1; num2 <= 9; num2++){
+                    if (num2 > num1){
+                        if (num1 == 8 && num2 == 9)
+                            Console.WriteLine("{0}{1}", num1, num2);
+                        else
+                            Console.Write("{0}{1}, ", num1, num2);
+                    }
+                }
+            }
+        }
+    }
+}
