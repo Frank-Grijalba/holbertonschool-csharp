e88e682f798f6b593d0922b574e3b7123e02442e
Frank-Grijalba <fjgrijalba@gmail.com>
2022-04-12 22:42:59 -0500

    tsk 12

--- 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -6,7 +6,20 @@ namespace _12_fizzbuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 1 ; i<= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                    Console.Write("FizzBuzz");
+                else if (i % 3 == 0)
+                    Console.Write("Fizz");
+                else if (i % 5 == 0)
+                    Console.Write("Buzz");
+                else
+                    Console.Write($"{i}");
+                if (i != 100)
+                    Console.Write(" ");
+            }
+            Console.Write("\n");
         }
     }
 }
