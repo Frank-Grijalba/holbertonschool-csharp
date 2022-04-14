c55d38edaff2968ed34f3674c558989b784ea26c
Frank-Grijalba <fjgrijalba@gmail.com>
2022-04-13 22:42:13 -0500

    tsk 7-15

--- 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -1,16 +1,15 @@
 ﻿using System;
 
-
 class Array
 {
     public static int[] ReplaceElement(int[] array, int index, int n)
     {
-        if (index > array.Length){
+        if (index < 0 || index > array.Length - 1){
             Console.WriteLine("Index out of range");
             return array;
         }
 
-        int newValue = array[index] = n;
+        array[index] = n;
         return array;
     }
 }
