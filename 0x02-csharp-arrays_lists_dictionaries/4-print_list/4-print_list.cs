376300e0f52e00a2297e1d9c1924f1ae180a786d
Frank-Grijalba <fjgrijalba@gmail.com>
2022-04-13 20:58:01 -0500

    4-6

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        int i;
+        List<int> new_list = new List<int>();
+        if (size < 0){
+            Console.WriteLine("Size cannot be negative");
+            return (null);
+        }
+        for (i = 0 ; i <= size - 1 ; i++){
+            new_list.Add(i);
+            if (i < size -1)
+                Console.Write($"{new_list[i]} ");
+            else
+                Console.Write(new_list[i]);
+        }
+        Console.Write('\n');
+        return(new_list);
+    }
+}
