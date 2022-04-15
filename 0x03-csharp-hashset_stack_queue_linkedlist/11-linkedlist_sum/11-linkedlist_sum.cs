31f9a45e2563db5f93248bbf21dbba8fdbda70c5
Frank-Grijalba <fjgrijalba@gmail.com>
2022-04-14 22:20:49 -0500

    fix tsk 10, 12

--- 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -1,20 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-
 class LList
 {
-    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n){
-        LinkedListNode<int> node = myLList.First;
-        if (node.Value >= n)
-            return myLList.AddFirst(n);
-        while (node.Next != null){
-            if (node.Next.Value >= n){
-                node = myLList.AddAfter(node, n);
-                return node;
-            }
-            node = node.Next;
-        }
-        node = myLList.AddLast(n);
-        return node;
+    public static int Sum(LinkedList<int> myLList){
+        int sum = 0;
+        foreach (var elem in myLList)
+            sum += elem;
+        return sum;
     }
 }
