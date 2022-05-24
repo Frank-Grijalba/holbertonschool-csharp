fd8e8b1395c6d9c50c7880b3126b018578a360c8
Frank-Grijalba <fjgrijalba@gmail.com>
2022-05-24 11:27:23 -0500

    Tsk 3

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class VectorMath
+{
+    public static double Magnitude(double[] vector){
+        double add = 0;
+        double result = 0;
+        if (vector.Length < 2 || vector.Length > 3)
+            return -1;
+
+        for (int i = 0; i < vector.Length; i++){
+            add += (Math.Pow(vector[i], 2));
+        }
+
+        result = Math.Ceiling(Math.Sqrt(add));
+        return result;
+    }
+}
