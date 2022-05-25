6aea6aed6347b451297094a26a51e0f87106f08f
Frank-Grijalba <fjgrijalba@gmail.com>
2022-05-25 04:29:16 -0500

    tsk 14

---
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/// <summary>This is the class object.</summary>
+class MatrixMath
+{
+    /// <summary>This is the class object.</summary>
+    public static double[,] Add(double[,] matrix1, double[,] matrix2)
+    {
+        if (
+            (matrix1.GetLength(0) != matrix1.GetLength(1)) ||
+            (matrix2.GetLength(0) != matrix2.GetLength(1)) ||
+            (matrix1.GetLength(0) != matrix2.GetLength(0)) ||
+            (matrix1.GetLength(1) != matrix2.GetLength(1))
+        )
+        {
+            return new double[,]{{-1}};
+        }
+
+        int matrix1_len = matrix1.GetLength(0);
+
+        for (int i = 0 ; i < matrix1_len ; i++)
+        {
+            for (int j = 0 ; j < matrix1_len ; j++)
+            {
+                matrix1[i, j] += matrix2[i, j];
+            }
+        }
+
+        return(matrix1);
+    }
+}
