b31b9d1b1a209bfaf9020ced35e0fd3cf095c6cf
Frank-Grijalba <fjgrijalba@gmail.com>
2022-05-25 03:43:06 -0500

    Tsk 7-9

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class VectorMath
+{
+     public static double[] Multiply(double[] vector, double scalar){
+        
+        double[] add = new double[2];
+        if (vector.Length < 2 || vector.Length > 3)
+        {
+            add[0] = -1;
+            add[1] = -1;
+            return add;
+        }
+        
+        double[] result = new double[vector.Length];
+        
+        for (int i = 0; i < vector.Length; i++){
+            result[i] = vector[i] * scalar;
+        }
+        
+        return result;
+    }
+}
